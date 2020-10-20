c4e9911aa7e3aa5febbe0a005cfa3240b79bf68a
vnod96 <vinod.suthar16@gmail.com>
2020-10-20 17:30:36 +0530

    Updates

---
+++ precache-manifest.850cdff65b08e25574691eaa9dbe23ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf87735a9190793b94bacebe7cec563",
+    "url": "/pac/index.html"
+  },
+  {
+    "revision": "365e07860f19634c8664",
+    "url": "/pac/static/css/main.21d002a0.chunk.css"
+  },
+  {
+    "revision": "a38983208bc58d42a1f4",
+    "url": "/pac/static/js/2.22d299de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pac/static/js/2.22d299de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365e07860f19634c8664",
+    "url": "/pac/static/js/main.5d780837.chunk.js"
+  },
+  {
+    "revision": "e9d26d7cba284675fc31",
+    "url": "/pac/static/js/runtime-main.b4030c31.js"
+  }
+]);
